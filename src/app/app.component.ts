32d353306dd5f2738fae891af4a39c7275fb22f0
collins kipkemoi <collinskipkemoi24@gmail.com>
2021-05-09 14:44:08 +0300

    paginator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material';
+import { merge, of } from 'rxjs';
+import { startWith, switchMap } from 'rxjs/operators';
+import { data } from './data';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'paginator';
+
+  dataSource: any[];
+  dataSize: number = data.length;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.linkListToPaginator();
+  }
+
+
+  linkListToPaginator() {
+    merge(this.paginator.page).pipe(
+      startWith({}),
+      switchMap(() => {
+        return of(data);
+      })
+    ).subscribe(res => {
+      const from = this.paginator.pageIndex * 10;
+      const to = from + 10;
+      this.dataSource = res.slice(from, to);
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// angular material
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatListModule} from '@angular/material/list';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatPaginatorModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
